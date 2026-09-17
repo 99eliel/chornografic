bda61c463d70c56efba7f2b25de5cea6cf845010
99eliel <131190282+99eliel@users.noreply.github.com>
2026-09-17 13:23:31 -0300

    Atualiza cache para novo relatório

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'story-ads-lab-v3';
+const CACHE_NAME = 'story-ads-lab-v4';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 
 self.addEventListener('install', event => {
