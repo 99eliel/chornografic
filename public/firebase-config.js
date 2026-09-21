1a96f067d976e4869fa0c0ab9f52d402c86d0a49
99eliel <131190282+99eliel@users.noreply.github.com>
2026-09-21 18:33:05 -0300

    Add Firebase web configuration

---
+++ public/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAEEbbKJ8XbNV8y2tKfg6vDrvzhnTwV1LY",
+  authDomain: "instagramanuncios.firebaseapp.com",
+  projectId: "instagramanuncios",
+  storageBucket: "instagramanuncios.firebasestorage.app",
+  messagingSenderId: "528452387615",
+  appId: "1:528452387615:web:95067c6852e4656a2b662a",
+  measurementId: "G-W4KLWWJ1ZL"
+};
